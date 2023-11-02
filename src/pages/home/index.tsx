a35dd604c6726cc67393f923cf112fc4c9400756
Nikhil Joshi <nikhilakjoshi@gmail.com>
2023-11-02 00:02:08 -0500

    1

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,23 @@
+import clsx from "clsx";
+import { Rubik } from "next/font/google";
+const font = Rubik({
+  subsets: ["latin-ext"],
+});
+
 export default function Home() {
   return (
     <>
-      <div className="px-4 py-2"></div>
-      <iframe
-        allow="web-share"
-        src="https://child-poc-iframe.vercel.app/home"
-        sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-storage-access-by-user-activation"
-        className="h-[50vh] w-[50dvw] border"
-      ></iframe>
+      <main className={clsx(font.className, "container mx-auto")}>
+        <div className="px-4 py-2 text-xl font-semibold">Parent Frame</div>
+        <div className="px-4 py-2">
+          <iframe
+            allow="web-share"
+            src="https://child-poc-iframe.vercel.app/home"
+            sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-storage-access-by-user-activation"
+            className="h-[45dvh] w-full border"
+          ></iframe>
+        </div>
+      </main>
     </>
   );
 }
