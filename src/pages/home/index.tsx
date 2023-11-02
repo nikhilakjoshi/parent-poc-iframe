f6a77ef28e752dfb9dbc4ca1963263ac2d274a5a
Nikhil Joshi <nikhilakjoshi@gmail.com>
2023-11-01 22:54:06 -0500

    1

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <>
       <div className="px-4 py-2"></div>
       <iframe
+        allow="web-share"
         src="https://child-poc-iframe.vercel.app/home"
         sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
         className="h-[50vh] w-[50dvw] border"
